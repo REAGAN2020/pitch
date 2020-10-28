26f2ddc211d5fdcc35e1123e785a13be5e7aca80
REAGAN2020 <reaganteflon@gmail.com>
2020-10-28 23:10:42 +0300

    my work

---
+++ start.sh
@@ -0,0 +1,5 @@
+export MAIL_USERNAME=reaganteflon@gmail.com
+export MAIL_PASSWORD=fenando99
+export SECRET_KEY=reagan123456789xyz
+export DATABASE_URL="postgres://moringa:Access@localhost:5432/reagan"
+python3.8 manage.py server
