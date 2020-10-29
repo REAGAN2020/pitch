2f18df95751932d5b30129dc4ce6495b33e0e3be
REAGAN2020 <reaganteflon@gmail.com>
2020-10-29 15:22:31 +0300

    fixed a bug

--- manage.py
+++ manage.py
@@ -29,4 +29,9 @@ def make_shell_context():
 
 
 if __name__ == '__main__':
+    app.secret_key = 'SECRET_KEY'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+
+    app.debug = True
     app.manage()
