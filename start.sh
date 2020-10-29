567deae96614e7f0354be727035e5099a67928b4
REAGAN2020 <reaganteflon@gmail.com>
2020-10-29 19:18:29 +0300

    added changes to work

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ export SECRET_KEY='reagan123456789xyz'
 export DATABASE_URL="postgresql+psycopg2://moringa:Access@localhost/reagan"
 python3.8 manage.py server  
 
-heroku config:set MAIL_USERNAME='reaganteflon@gmail.com' && heroku config:set MAIL_PASSWORD='fenando99' && heroku config:set SECRET_KEY='reagan123456789xyz'
+# heroku config:set MAIL_USERNAME='reaganteflon@gmail.com' && heroku config:set MAIL_PASSWORD='fenando99' && heroku config:set SECRET_KEY='reagan123456789xyz'
