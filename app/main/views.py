0c081b792733fc7703914bc2eeed3ad32f115bf2
REAGAN2020 <reaganteflon@gmail.com>
2020-10-29 03:00:49 +0300

    app folders and files

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,21 @@ from .forms import PitchForm, CommentForm, UpdateProfile, CategoryForm
 from .. import db, photos
 from . import main
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 
 @main.route ('/')
 def index():
@@ -12,7 +27,7 @@ def index():
     Function that returns index page and data
     """
     category = PitchCategory.get_categories()
-    return render_template('index.html', categories = category)
+    return render_template('index.html', categories = category ,posts=posts)
 
 @main.route('/category/new-pitch/<int:id>', methods= ['GET', 'POST'])
 @login_required
