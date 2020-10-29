e9e595a6c5e76ff7c937b9243115b10bcd20eb7e
REAGAN2020 <reaganteflon@gmail.com>
2020-10-29 04:20:31 +0300

    changes

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SQLALCHEMY_DATABASE_URI="postgres://moringa:Access@localhost:5000/reagan"
+    SQLALCHEMY_DATABASE_URI="postgres://moringa:Access@localhost/reagan"
     print (SQLALCHEMY_DATABASE_URI, "00000")
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -31,7 +31,7 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_test'
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI="postgres://moringa:Access@localhost:5000/reagan"
+    SQLALCHEMY_DATABASE_URI="postgres://moringa:Access@localhost/reagan"
     DEVELOPMENT = True
     DEBUG = True
 config_options = {
